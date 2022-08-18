8857c8bd4c71eed2f1c7cbe54b2f9131a894f840
William Hutchinson <willhutchinson2.0@gmail.com>
2022-08-18 15:03:26 -0400

    feat: Basic hook

---
+++ src/index.ts
@@ -0,0 +1,62 @@
+export let api: string;
+
+export async function emailCode(email: string): Promise<boolean> {
+  const res = await fetch(`${api}`, {
+    method: 'POST',
+    body: JSON.stringify({
+      query: `"mutation:{
+        login(input:{email:\"${email}\"})
+      }"`,
+    }),
+  });
+
+  return await res.json();
+}
+
+export async function verifyCode(
+  email: string,
+  code: string,
+): Promise<{sessionId: string; permissions: string[]}> {
+  const res = await fetch(`${api}`, {
+    method: 'POST',
+    body: JSON.stringify({
+      query: `"mutation:{
+        verify(input:{email:\"${email}\",code:\"${code}\"}){
+          sessionId
+          permissions
+        }
+      }"`,
+    }),
+  });
+  return await res.json();
+}
+
+export async function permissions(): Promise<string[]> {
+  const res = await fetch(`${api}`, {
+    method: 'POST',
+    body: JSON.stringify({
+      query: `{
+        permissions
+      }`,
+    }),
+  });
+  return await res.json();
+}
+
+export async function logout(): Promise<boolean> {
+  const res = await fetch(`${api}`, {
+    method: 'POST',
+    body: JSON.stringify({
+      query: `"mutation:{
+        logout()
+      }"`,
+    }),
+  });
+
+  return await res.json();
+}
+
+export function useMacroAuth(endpoint: string) {
+  api = endpoint;
+  return {emailCode, verifyCode, permissions, logout};
+}
