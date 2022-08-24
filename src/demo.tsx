720df315bcb1bd8612a206312f95ef54541141b4
Sean Aye <hello@seanaye.ca>
2022-08-24 18:05:43 -0400

    update the demo app to query permissions

--- src/demo.tsx
+++ src/demo.tsx
@@ -1,26 +1,44 @@
-import { createResource, createSignal } from "solid-js";
+import { createResource, createSignal, Resource } from "solid-js";
 import { render } from "solid-js/web";
 import { createClient } from "./index";
 
 const [email, setEmail] = createSignal("");
 const client = createClient("http://localhost:8080/graphql");
 
+function formSubmit(
+  ref: HTMLFormElement,
+  accessor: () => ((ref: HTMLFormElement) => any) | undefined
+) {
+  const cb = accessor() ?? (() => {})
+  ref.setAttribute("novalidate", "");
+  ref.onsubmit = async (e) => {
+    e.preventDefault();
+    return await Promise.resolve(cb(ref));
+  };
+}
+
+function Result(props: { data: Resource<any> }) {
+  return (
+    <div>
+      <div>result: {JSON.stringify(props.data())}</div>
+      <div>loading: {JSON.stringify(props.data.loading)}</div>
+      <div>error: {JSON.stringify(props.data.error)}</div>
+    </div>
+  );
+}
+
 function VerifyInput() {
   const [code, setCode] = createSignal("");
   const buttonEnabled = () => code() && email();
-  const [params, setParams] = createSignal<null| {email: string; code: string}>(null)
+  const [params, setParams] = createSignal<null | {
+    email: string;
+    code: string;
+  }>(null);
 
   const [data] = createResource(params, client.verifyCode);
 
-  const handleSubmit = (e: Event) => {
-    e.preventDefault()
-    const param = { email: email(), code: code() }
-    console.log(param)
-    setParams(param);
-  }
-
   return (
-    <form onSubmit={handleSubmit}>
+    <form use:formSubmit={() => setParams({ email: email(), code: code() })}>
       <input
         type="text"
         placeholder="email"
@@ -36,18 +54,29 @@ function VerifyInput() {
       <button disabled={!buttonEnabled} type="submit">
         Submit
       </button>
-      <div>result: {JSON.stringify(data())}</div>
-      <div>loading: {JSON.stringify(data.loading)}</div>
-      <div>error: {JSON.stringify(data.error)}</div>
+      <Result data={data} />
+    </form>
+  );
+}
+
+function Permissions() {
+  const [data, { refetch }] = createResource(() => client.permissions({}));
+
+  return (
+    <form use:formSubmit={() => refetch()}>
+      <button type="submit">Refetch</button>
+      <Result data={data} />
     </form>
   );
 }
 
 function App() {
-  return <div>
-    Hello World
-    <VerifyInput />
-  </div>;
+  return (
+    <div>
+      <VerifyInput />
+      <Permissions />
+    </div>
+  );
 }
 
 render(() => <App />, document.getElementById("app") as HTMLElement);
